040d0ecdfb446cdfca1fbf9e81e07db300763047
iqbal-hossain <mdiqbalhossain5170@gmail.com>
2022-07-31 15:49:55 +0600

    Home page and sanity section added

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { motion } from 'framer-motion';
+
+import { images } from '../../constants';
 
 import './About.scss';
+import {urlFor, client} from '../../client';
 
 const About = () => {
+  
+  const [abouts, setAbouts] = useState([]);
+  console.log(abouts);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
+  }, []);
+  
+  
   return (
-    <div>About</div>
+    <>
+      <h2 className="head-text"> I know that <span>Good dev</span> <br /> means <span>Good bussiness</span>
+      </h2>
+
+      <div className="app__profiles">
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: "tween" }}
+            className="app__profile-item"
+            key={about.title + index}
+          >
+            <img src={about.imgURL} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
+            <h2 className="p-text" style={{ marginTop: 10 }}>{about.description}</h2>
+          </motion.div>
+        ))}
+      </div>
+    </>
   )
 }
 
